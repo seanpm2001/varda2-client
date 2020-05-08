904d3a3d89b183812c25c8c2c9a8adaf217d96ae
Mark Santcroos <m.a.santcroos@lumc.nl>
2020-05-08 13:22:17 +0200

    Capture wrong token.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='varda2-client',
-    version='0.7',
+    version='0.8',
     description='A python CLI to Varda2 frequency database server.',
     url='http://github.com/varda/varda2-client',
     author='Mark Santcroos',
--- varda2_client/__init__.py
+++ varda2_client/__init__.py
@@ -301,9 +301,11 @@ def upload_helper(session, proto, server, filename, lab_sample_id, disease_code,
             if verbose:
                 print("done!", file=sys.stderr)
         except requests.exceptions.HTTPError as err:
-            if verbose:
+            if 'msg' in resp.json() and resp.json()['msg'] == 'Signature verification failed':
+                raise SystemExit('Invalid token')
+            elif verbose:
                 print("failed!", file=sys.stderr)
-            raise SystemExit(err)
+                raise SystemExit(err)
 
     return remote_var_fn
 
